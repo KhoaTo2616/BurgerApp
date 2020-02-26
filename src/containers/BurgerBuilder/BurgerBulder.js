a549e747eb5af157fe10ba655b42bf8b20854282
tong2616 <tong@sheridancollege.ca>
2020-02-26 14:21:49 -0500

    Added routing for the app and checkout page

--- src/containers/BurgerBuilder/BurgerBulder.js
+++ src/containers/BurgerBuilder/BurgerBulder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios
       .get("https://burger-app-87a25.firebaseio.com/ingredients.json")
       .then(response => {
@@ -88,35 +89,22 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // alert("You continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      /**
-       * For the real life application, the price should
-       * be calculated on the server to avoid user manipulating data
-       * before sending it
-       */
-      customer: {
-        name: "Kevin To",
-        address: {
-          street: "Random",
-          postalCode: "L11 Z22",
-          country: "Canada"
-        },
-        email: "example@gmail.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    queryParams.push("price=" + this.state.totalPrice);
+    const queryString = queryParams.join("&");
+    console.log(queryString);
+
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
+    });
   };
 
   render() {
