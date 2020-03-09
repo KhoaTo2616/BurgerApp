7358558e09f517a5777e7e88fa7157f132fd8d76
tong2616 <tong@sheridancollege.ca>
2020-03-09 13:05:13 -0400

    Added Login Form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,8 @@ const navigationItems = () => (
     <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>{" "}
+    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Authenticate</NavigationItem>
   </ul>
 );
 
